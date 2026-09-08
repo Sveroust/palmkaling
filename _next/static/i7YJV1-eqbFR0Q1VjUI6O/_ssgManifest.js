474931b3cc1e7bebbec799b09e269ac38d4310b9
Sveroust <kthoriq7@gmail.com>
2026-09-08 21:11:33 +0700

    Deploy 2026-09-08 21:11

---
+++ _next/static/i7YJV1-eqbFR0Q1VjUI6O/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fexport-process","\u002F[locale]\u002Fproduct","\u002F[locale]\u002Fsupply-network"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
